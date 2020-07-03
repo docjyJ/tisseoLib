6c225efabf510c5ef07d7f1403a822e1b10a5b3d
docjyJ <docteurjeanyves@gmail.com>
2020-07-03 14:37:10 +0200

    update exception

--- src/main/kotlin/fr/docjyJ/tisseoLib/exception/TisseoClientException.kt
+++ src/main/kotlin/fr/docjyJ/tisseoLib/exception/TisseoClientException.kt
@@ -6,7 +6,8 @@ package fr.docjyJ.tisseoLib.exception
 class TisseoClientException internal constructor(message: String, cause: Throwable) : Exception(message, cause) {
 
     companion object{
-        internal const val ENCODE_PARAMETER = "Unable to encode the parameters."
+        internal const val ENCODE_DATE = "Unable to encode the date parameter."
+        internal const val ENCODE_PARAMETER = "Unable to encode the parameter."
         internal const val PARSE_OBJECT = "Unable to parse the response."
         internal const val READ_RESPONSE = "Unable to read the response."
         internal const val CREATE_REQUEST = "Unable to create the request."
